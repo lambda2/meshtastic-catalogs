f3154b168a7ca30961045bf4b8b47798672a2c72
André Aubin <andre@andral.xyz>
2025-01-16 18:45:06 +0100

    feat: add diff

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import type { CatalogDefinition } from "./types";
 import deepmerge from "deepmerge";
 
+import catalogs from "./catalogs/index";
+import { detailedDiff } from "deep-object-diff";
+
 export const applyCatalog = (
-  currentConfig: CatalogDefinition,
-  catalogToApply: CatalogDefinition,
+	currentConfig: CatalogDefinition,
+	catalogToApply: CatalogDefinition,
 ) => {
-  return deepmerge<CatalogDefinition>(currentConfig, catalogToApply);
+	return deepmerge<CatalogDefinition>(currentConfig, catalogToApply);
+};
+
+export const diffCatalog = (
+	currentConfig: CatalogDefinition,
+	catalogToCompare: CatalogDefinition,
+) => {
+	return detailedDiff(currentConfig, catalogToCompare);
 };
 
 export const getCatalogs = async () => {
-  const data = await import("./catalogs/index");
-  console.log(data);
-
-  return data;
+	return catalogs;
 };
+
+export { catalogs };
