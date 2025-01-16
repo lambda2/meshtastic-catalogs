863d63aa4421dda13480f0ad3a42ebf5dba879c1
André Aubin <andre@andral.xyz>
2025-01-16 18:30:00 +0100

    chore: types

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import type { CatalogDefinition } from "./types";
 import deepmerge from "deepmerge";
 
 export const applyCatalog = (
-	currentConfig: CatalogDefinition,
-	catalogToApply: CatalogDefinition,
+  currentConfig: CatalogDefinition,
+  catalogToApply: CatalogDefinition,
 ) => {
-	return deepmerge<CatalogDefinition>(currentConfig, catalogToApply);
+  return deepmerge<CatalogDefinition>(currentConfig, catalogToApply);
 };
 
 export const getCatalogs = async () => {
-	const data = await import("./catalogs/index");
-	console.log(data);
+  const data = await import("./catalogs/index");
+  console.log(data);
 
-	return data;
+  return data;
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,20 +3,20 @@ import { applyCatalog, getCatalogs } from "../src/index";
 import * as Catalogs from "../src/catalogs";
 
 describe("#applyCatalog", () => {
-	test("can apply empty config", () => {
-		const initial = { metadata: { name: "empty" } };
-		const toApply = Catalogs.DefaultSimple_868;
-		const final = applyCatalog(initial, toApply);
-		expect(final).toMatchObject(toApply);
-	});
+  test("can apply empty config", () => {
+    const initial = { metadata: { name: "empty" } };
+    const toApply = Catalogs.DefaultSimple_868;
+    const final = applyCatalog(initial, toApply);
+    expect(final).toMatchObject(toApply);
+  });
 });
 
 describe("#getCatalogs", () => {
-	test("can get catalogs list", async () => {
-		const catalogs = await getCatalogs();
+  test("can get catalogs list", async () => {
+    const catalogs = await getCatalogs();
 
-		expect(catalogs.default.length).toBeGreaterThan(0);
-		expect(catalogs.DefaultSimple_868).not.toBeNull();
-		expect(catalogs.DefaultSimple_868.metadata.name).not.toBeNull();
-	});
+    expect(catalogs.default.length).toBeGreaterThan(0);
+    expect(catalogs.DefaultSimple_868).not.toBeNull();
+    expect(catalogs.DefaultSimple_868.metadata.name).not.toBeNull();
+  });
 });
